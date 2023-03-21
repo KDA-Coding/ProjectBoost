946b43d585d5f0f7fb4702dad72aac66ada3b565
Kirk Allgeyer <59184838+KDA-Coding@users.noreply.github.com>
2023-03-21 12:13:50 -0400

    Testing Movement Inputs
    Added Movement Script with two methods to handle Thrust force upward and left/right rotation.
    
    Test cases successful. Proceeding to implement physics force for "thrust" movement.

--- Blaster Boring/Assets/Scripts/Movement.cs
+++ Blaster Boring/Assets/Scripts/Movement.cs
@@ -13,6 +13,29 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressed Spacebar. Thrust Active");
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A. Rotating Counter-Clockwise around Z");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed D. Rotating Clockwise around Z");
+        }
     }
 }
+
+    
